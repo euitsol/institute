2ad37f62d34b54dd2585e5e64a3d833bdfd8591a
Shetu <shetu.euitsols@gmail.com>
2020-02-25 19:40:11 +0600

    Due View Change

--- resources/views/report/due_payment_students.blade.php
+++ resources/views/report/due_payment_students.blade.php
@@ -12,7 +12,7 @@
             <div class="col-md-10 col-lg-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4> <span class="text-danger">Due</span> Report </h4>
+                        <h4><span class="text-danger">Due</span> Report </h4>
                     </div>
 
                     <div class="card-body">
@@ -26,65 +26,75 @@
                                 {{ session('error') }}
                             </p>
                         @endif
-
-                        <h5 class="mb-4">Course Type : <span class="font-weight-normal">{{$course_type}}</span></h5>
-
-                        @if (count($students) > 0)
-                                <table class="table table-bordered text-center">
-                                    <tr>
-                                        <th rowspan="2" class="align-middle">SL</th>
-                                        <th rowspan="2" class="align-middle">Name</th>
-                                        <th rowspan="2" class="align-middle">Phone</th>
-                                        <th colspan="4" class="align-middle">Details</th>
-                                    </tr>
-                                    <tr>
-                                        <th>Course</th>
-                                        <th>Batch</th>
-                                        <th>Due</th>
-                                        <th>Paid</th>
-                                    </tr>
-                                    @php($total_paid = 0)
-                                    @php($total_due = 0)
-                                    @foreach ($students as $sk => $student)
-                                        <tr>
-                                            <td rowspan="{{$student->courses->count()}}" class="align-middle">{{ ++$sk }}</td>
-                                            <td rowspan="{{$student->courses->count()}}"
-                                                class="align-middle">{{ $student->name }}</td>
-                                            <td rowspan="{{$student->courses->count()}}"
-                                                class="align-middle">{{$student->phone}}</td>
-                                            @foreach($student->courses as $course)
-                                                @if ($course->due_status)
-                                                    @if ($loop->first)
-
-                                                        @php($total_paid += $course->paid_amount)
-                                                        @php($total_due += $course->due_amount)
-
-                                                        <td>{{$course->title}}</td>
-                                                        <td>{{$course->batch}}</td>
-                                                        <td>{{$course->due_amount}}</td>
-                                                        <td>{{$course->paid_amount}}</td>
-                                        </tr>
-                                        @else
-
-                                            @php($total_paid += $course->paid_amount)
-                                            @php($total_due += $course->due_amount)
-
-                                            <tr>
-                                                <td>{{$course->title}}</td>
-                                                <td>{{$course->batch}}</td>
-                                                <td>{{$course->due_amount}}</td>
-                                                <td>{{$course->paid_amount}}</td>
-                                            </tr>
-                                        @endif
-                                        @endif
-                                    @endforeach
-                                    @endforeach
-                                </table>
-
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h5 class="mb-4">Course Type : <span class="font-weight-normal">{{$course_type}}</span>
+                                </h5>
+                            </div>
+                            <div class="col-md-6">
                                 <p class="text-right">
-                                    <b>Total Paid :</b> {{$total_paid}} <br>
-                                    <b>Total Due :</b> {{$total_due}}
+                                    <b>Total Paid :</b> <span id="total_paid"></span> <br>
+                                    <b>Total Due :</b> <span id="total_due"></span>
                                 </p>
+                            </div>
+                        </div>
+                        @if (count($students) > 0)
+                            <table class="table table-bordered text-center">
+                                <tr>
+                                    <th rowspan="2" class="align-middle">SL</th>
+                                    <th rowspan="2" class="align-middle">Name</th>
+                                    <th rowspan="2" class="align-middle">Phone</th>
+                                    <th colspan="4" class="align-middle">Details</th>
+                                </tr>
+                                <tr>
+                                    <th>Course</th>
+                                    <th>Batch</th>
+                                    <th>Due</th>
+                                    <th>Paid</th>
+                                </tr>
+                                @php($total_paid = 0)
+                                @php($total_due = 0)
+                                @foreach ($students as $sk => $student)
+                                    <tr>
+                                        <td rowspan="{{$student->courses->count()}}"
+                                            class="align-middle">{{ ++$sk }}</td>
+                                        <td rowspan="{{$student->courses->count()}}"
+                                            class="align-middle">{{ $student->name }}</td>
+                                        <td rowspan="{{$student->courses->count()}}"
+                                            class="align-middle">{{$student->phone}}</td>
+                                        @foreach($student->courses as $course)
+                                            @if ($course->due_status)
+                                                @if ($loop->first)
+
+                                                    @php($total_paid += $course->paid_amount)
+                                                    @php($total_due += $course->due_amount)
+
+                                                    <td>{{$course->title}}</td>
+                                                    <td>{{$course->batch}}</td>
+                                                    <td>{{$course->due_amount}}</td>
+                                                    <td>{{$course->paid_amount}}</td>
+                                    </tr>
+                                    @else
+
+                                        @php($total_paid += $course->paid_amount)
+                                        @php($total_due += $course->due_amount)
+
+                                        <tr>
+                                            <td>{{$course->title}}</td>
+                                            <td>{{$course->batch}}</td>
+                                            <td>{{$course->due_amount}}</td>
+                                            <td>{{$course->paid_amount}}</td>
+                                        </tr>
+                                    @endif
+                                    @endif
+                                @endforeach
+                                @endforeach
+                            </table>
+
+                            <p class="text-right" style="display: none;">
+                                <span id="total-paid">{{$total_paid}}</span>
+                                <span id="total-due">{{$total_due}}</span>
+                            </p>
                         @endif
 
                     </div>
@@ -96,5 +106,10 @@
 @endsection
 
 @push('js')
-
+    <script>
+        $(function () {
+            $('#total_paid').html($('#total-paid').html());
+            $('#total_due').html($('#total-due').html());
+        });
+    </script>
 @endpush
